d7b17b8016884a27a64389a42e179bc2199f273a
fomox <magnugut@stud.ntnu.no>
2017-10-23 00:13:21 +0200

    added example with analyzerregister and analyzeeconomicconnections on how this should be used. also applies to datasources

--- src/controllers/AnalyzerRegister.java
+++ src/controllers/AnalyzerRegister.java
@@ -10,10 +10,15 @@ public class AnalyzerRegister {
     private List<AnalyzeDataInterface> adList;
 
     public AnalyzerRegister() {
+        //adds the analyzer to the list over analyzers. this way we can use it later
+        adList.add(new AnalyzeEconomicConnections());
+
         throw new UnsupportedOperationException("not implemented");
     }
 
     void getDataFromAll(Candidate candidate) {
+        //here the idea is that we loop over everything in adList and runs the analyzer
+
         throw new UnsupportedOperationException("not implemented");
     }
 

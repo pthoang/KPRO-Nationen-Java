c4838b38c6140cc7596fce9a1b0925329eff8853
Dora Oline Eriksrud <doraoline@gmail.com>
2017-11-22 20:26:59 +0100

    Removed println

--- src/model/AmazonBucketUploader.java
+++ src/model/AmazonBucketUploader.java
@@ -123,7 +123,6 @@ public class AmazonBucketUploader {
 	}
 
     public BufferedImage getImageFromBucket(String imageName) {
-        String imageKey = folderName + "/images/" + imageName;
         System.out.println("ImageKey: " + imageKey);
         GetObjectRequest getObjReq = new GetObjectRequest(bucketName, imageKey);
         File file = new File("tempFiles/" + imageKey);
